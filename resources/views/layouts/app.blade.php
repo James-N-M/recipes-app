bf3347a76c47eb6c211b01e70e4b29006294dfcc
James Moore <jamesm0025@gmail.com>
2019-03-25 20:30:48 -0400

    asset css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
-    <link href="{{ ('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
